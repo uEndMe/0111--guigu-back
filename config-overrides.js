476f3ac89ecb3c7f85abbefff823039bcb6a2261
water <695381719@qq.com>
2020-01-12 18:52:36 +0800

    完成基本配置antd,ES7,alias等...

---
+++ config-overrides.js
@@ -0,0 +1,27 @@
+const {
+  override,
+  fixBabelImports,
+  addLessLoader,
+  addDecoratorsLegacy,
+  addWebpackAlias,
+} = require('customize-cra');
+
+module.exports = override(
+  //按需加载
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  //自定义主题
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { '@primary-color': '#1DA57A' },
+  }),
+  //ES7 装饰器语法兼容
+  addDecoratorsLegacy(),
+  //配置webpack路径别名
+  addWebpackAlias({
+
+  }),
+);
