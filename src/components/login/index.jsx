0d7f069f3c76fa4228bc6acacf53647ec25b3f41
water <695381719@qq.com>
2020-01-13 19:08:22 +0800

    finish: 添加了表单校验成功

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,26 +1,57 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import logo from './logo.png';
 import './index.less';
 
-import { Form, Input, Button, Icon } from 'antd';
+import { Form, Input, Button, Icon, message } from 'antd';
 
-// @From.create()
+@Form.create()
 class Login extends Component {
+  //登录请求
+  login = e => {
+    e.preventDefault();
+    //验证字段 (发送请求，及响应)
+    this.props.form.validateFields((err, values) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const { username, password } = values;
+      axios.post({ username, password })
+        .then(response => {
+          console.log(response);
+          if (response.data.status === 0) {
+            this.props.history.replace('/');
+          } else {
+            message.error(response.data.msg);
+            this.props.form.resetFields(['password']);
+          }
+        })
+        .catch(e => {
+          console.log(e);
+          message.error('网络错误~');
+        });
+    });
+  }
+
+  //表单校验
   validator = (rule, value, callback) => {
     const name = rule.field === 'username' ? '用户名' : '密码';
     if (!value) {
       callback(`请输入${name}`);
-    } else if (value.length < 4) {
-      callback(`${name}必须大于4位`)
-    } else if (value.length > 15) {
-      callback(`${name}必须大于15位`)
+    } else if (value.length < 4 || value.length > 15) {
+      callback(`${name}必须大于4位，小于15位`)
     } else if (!/^\w+/.test(value)) {
       callback(`${name}只能包含英文，数字，下划线`)
+    } else {
+      callback();
     }
   }
+
   render() {
     const { getFieldDecorator } = this.props.form;
+    const { Item } = Form;
     return <section className="login">
       <header className="loginHead">
         <img src={logo} alt="尚硅谷" />
@@ -28,34 +59,32 @@ class Login extends Component {
       </header>
       <article className="loginBox">
         <h3>用户登录</h3>
-        <Form className="loginForm">
-          <Form.Item>
+        <Form className="loginForm" onSubmit={this.login}>
+          <Item>
             {getFieldDecorator(
               'username',
               { rules: [{ validator: this.validator }] }
             )(
-              <Input
-                type="text"
+              <Input type="text"
                 prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                 placeholder="用户名"
               />
             )}
-          </Form.Item>
-          <Form.Item>
+          </Item>
+          <Item>
             {getFieldDecorator(
               'password',
               { rules: [{ validator: this.validator }] }
             )(
-              <Input
-                type="password"
+              <Input type="password"
                 prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
                 placeholder="密码"
               />
             )}
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary">登录</Button>
-          </Form.Item>
+          </Item>
+          <Item>
+            <Button type="primary" htmlType='submit'>登录</Button>
+          </Item>
         </Form>
       </article>
     </section>
@@ -63,5 +92,5 @@ class Login extends Component {
 }
 
 //高阶组件，给 Login 传递 from 属性
-export default Form.create()(Login);
-// export default Login;
+// export default Form.create()(Login);
+export default Login;
