322a3ed4b279fb89f32315ae29e04642926a075f
water <695381719@qq.com>
2020-01-17 10:55:52 +0800

    finish: category static component

--- src/components/category/index.jsx
+++ src/components/category/index.jsx
@@ -4,17 +4,25 @@ import { Card, Button, Icon, Table } from 'antd';
 import { columns, data } from '$datas/category';
 
 export default class Category extends Component {
+
   render() {
     return <div>
       <Card title="分类列表" extra={
         <Button type="primary">
           <Icon type="plus" />
+          分类列表
         </Button>
       }>
         <Table
           columns={columns}
           dataSource={data}
           bordered
+          pagination={{
+            showSizeChanger: true,  //页数改变
+            showQuickJumper: true,  //快速跳转
+            pageSizeOptions: ['3', '6', '9', '12'],
+            defaultPageSize: 3,
+          }}
         />
       </Card>
     </div>
--- src/datas/category.js
+++ src/datas/category.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import { Button } from 'antd';
+
+
+
 export const columns = [
   {
     title: '品类名称',
@@ -5,33 +10,49 @@ export const columns = [
   },
   {
     title: '操作',
+    width: '33.33%',
     dataIndex: 'operation',
+    render: () => {
+      return <div>
+        <Button type='link' style={{ marginLeft: -15, }} >修改</Button>
+        <Button type='link' >删除</Button>
+      </div>
+    }
   },
 ]
 
 export const data = [
   {
+    key: 1,
     categoryName: '鼠标',
   },
   {
+    key: 2,
     categoryName: '键盘',
   },
   {
+    key: 3,
     categoryName: '显示屏',
   },
   {
+    key: 4,
     categoryName: '移动硬盘',
   },
   {
+    key: 5,
     categoryName: 'CPU',
   },
   {
+    key: 6,
     categoryName: '内存条',
   },
   {
+    key: 7,
     categoryName: 'U盘',
   },
   {
+    key: 8,
     categoryName: '电源',
   },
 ];
+
